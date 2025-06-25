caafcddbf194edda64527abecf15295136482071
Thibaut <tdnet59@gmail.com>
2025-06-25 13:11:25 +0000

    feature(payment): working transaction

--- app/app/(authenticated)/_layout.tsx
+++ app/app/(authenticated)/_layout.tsx
@@ -1,40 +1,16 @@
-import { Button } from "@/components/ui/button";
-import { P } from "@/components/ui/typography";
-import colors from "@/constants/colors";
-import { useAuth } from "@/context/auth-context";
-import { Redirect, Stack, Tabs } from "expo-router";
-import { HouseIcon, SendIcon } from "lucide-react-native";
-import { StatusBar } from "react-native";
-import TabBar from "../../components/tab-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
 
-export default function LoggedInLayout() {
-    const { user, isLoading } = useAuth();
-
-    if (isLoading) {
-        return null;
-    }
-
-    if (!user) {
-        return <Redirect href='/sign-in' />
-    }
-
+export default function Layout() {
     return (
-        <SafeAreaView style={{ flex: 1 }} edges={['bottom', 'left', 'right']}>
-            <Tabs screenOptions={{ headerShown: false }} tabBar={(props) => <TabBar {...props} />}>
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        title: 'Accueil',
-                    }}
-                />
-                <Tabs.Screen
-                    name="payment"
-                    options={{
-                        title: 'Payer',
-                    }}
-                />
-            </Tabs>
-        </SafeAreaView>
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="modals"
+                options={{
+                    presentation: 'modal',
+                    headerShown: false
+                }}
+            />
+        </Stack>
     );
 }
